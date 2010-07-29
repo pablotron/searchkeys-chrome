f4114a67544a6c1457a7b46f68edab0a9faed3bc
pabs <pabs@pablotron.org>
2010-07-29 06:02:43 -0400

    working fine now

--- searchkeys.js
+++ searchkeys.js
@@ -1,33 +1,114 @@
 (function () {
-  var KEY_CODES = {
-    zero:   '0'.charCodeAt(0),
-    dot:    '.'.charCodeAt(0),
-    comma:  ','.charCodeAt(0)
-  };
-
-  var SPAN_HTML = { 
-    pre:  "<span class='search-key'>",
-    post: "</span>",
-  }
-
   // "global" state
   var active = false,
       links = [];
 
+  /******************/
+  /* util functions */
+  /******************/
+
   /**
-   * on - attach override event to document
+   * click - click selected element.
    */
-  function on(el, ev, fn, capture) {
-    // return el.addEventListener(ev, fn, !!capture);
-    return el.addEventListener(ev, fn, !!capture);
+  function click(el) {
+    // build click event
+    var ev = document.createEvent('MouseEvents');
+    ev.initEvent('click', true, false);
+
+    // fire event
+    el.focus();
+    el.dispatchEvent(ev);
   }
 
-  function mangle(el, i) {
-    var e = document.createElement("span"),
+  /***********/
+  /* actions */
+  /***********/
+
+  /**
+   * prev - go to previous page of links.
+   */
+  function prev() {
+    var els = document.querySelectorAll('a.pn');
+
+    if (els.length > 1)
+      click(els[0]);
+  }
+
+  /**
+   * next - go to next page of links.
+   */
+  function next() {
+    var els = document.querySelectorAll('a.pn');
+
+    if (els.length > 0)
+      click(els[els.length - 1]);
+  }
+
+  /**
+   * jump - select Nth link on page.
+   */
+  function jump(ofs) {
+    click(links[ofs]);
+  }
+
+  /******************/
+  /* init functions */
+  /******************/
+
+  // event callbacks
+  var CALLBACKS = {
+    // disable key monitoring if any element is focused
+    blur: function() {
+      // enable keypress monitoring
+      active = true;
+    },
+
+    // enable key monitoring if any element is blurred
+    focus: function() {
+      // disable keypress monitoring
+      active = false;
+    },
+
+    // monitor keypress events for navigation keys
+    keypress: function(ev) {
+      // skip keypress if we're focused on a text field or a 
+      // modifier key is pressed
+      if (!active || ev.altKey || ev.metaKey || ev.ctrlKey)
+        return;
+
+      // get key code
+      var kc = ev.which;
+
+      if (kc == ','.charCodeAt(0)) {
+        prev();
+      } else if (kc == '.'.charCodeAt(0)) {
+        next();
+      } else {
+        var o = kc - '0'.charCodeAt(0);
+
+        // if key is navkey (0-9), then jump to link
+        if (o >= 0 && o <= 9) {
+          jump(o ? o - 1 : 9);
+        } else {
+          // unknown key - ignore event
+          return;
+        }
+      }
+
+      // stop event
+      return false;
+    }
+  };
+
+  /**
+   * add - add link to list of links
+   */
+  function add(el, i) {
+    var e = document.createElement('span'),
         k = (i + 1) % 10;
 
     // update tmp element
-    e.innerHTML = SPAN_HTML.pre + k + SPAN_HTML.post;
+    e.innerHTML = "<span class='search-key'>" + k + "</span>";
 
     // add tmp element to dom
     el.parentNode.appendChild(e);
@@ -36,123 +117,33 @@
     links.push(el);
   }
 
+  /**
+   * init - find navigation links and bind them to keys
+   */
   function init() {
     var i, l, els;
 
-    // get list of matching elements
+    // get list of search result links
     els = document.querySelectorAll('h3.r a.l');
 
     // get number of search results on this page
     l = (els.length < 10) ? els.length : 10;
 
-    // iterate over each result
+    // add links
     for (i = 0; i < l; i++)
-      mangle(els[i], i);
+      add(els[i], i);
 
-    // return whether we had any matches
-    return l > 0;
-  }
+    // if we had matches, then bind event handlers
+    if (l > 0) {
+      // bind to relevant event handlers
+      for (var k in CALLBACKS)
+        document.addEventListener(k, CALLBACKS[k], true);
 
-  function enable() {
-    active = true;
-  }
-
-  function disable() {
-    active = false;
-  }
-
-  function click(el) {
-    var ev = document.createEvent('MouseEvents');
-    ev.initEvent('click', true, false);
-
-    el.focus();
-    el.dispatchEvent(ev);
-  }
-
-  function prev() {
-    var els = document.querySelectorAll('a.pn');
-    if (els.length > 0) {
-      // location = els[0].href;
-      click(els[0]);
+      // enable key monitoring
+      active = true;
     }
   }
 
-  function next() {
-    var els = document.querySelectorAll('a.pn');
-      alert('next');
-    if (els.length > 1) {
-      // location = els[0].href;
-      click(els[1]);
-    }
-  }
-
-  function index_of(ev) {
-    var k = ev.which - KEY_CODES.zero;
-
-    if (k >= 0 && k <= 9)
-      return (k == 0) ? 9 : k - 1;
-
-    return -1;
-  }
-
-  function dump_key_event(ev) {
-    console.log([
-      'keyCode = '  + code,
-      'keyIdentifier = '  + ev.keyIdentifier,
-      'keyIdentifiercc = '  + ev.keyIdentifier.charCodeAt(0),
-      'which = '    + ev.which,
-      'textInput = '    + ev.textInput,
-      'charCode = ' + ev.charCode 
-    ].join(', '));
-  }
-
-  function keydown(ev) {
-    if (!active || ev.altKey || ev.metaKey || ev.ctrlKey)
-      return;
-
-    var id = ev.keyIdentifier;
-
-    if (id == '.') {
-      next();
-    } else if (id == ',') {
-      prev();
-    } else {
-      // get link offset
-      var ofs = index_of(ev);
-
-      // if offset is valid, then handle link
-      if (ofs != -1) {
-        // location = links[ofs];
-        click(links[ofs]);
-        return false;
-      }
-    }
-  }
-
-  function wrap() {
-/* 
- *     var k, cbs = { focus: disable, blur: enable, keydown: keydown };
- * 
- *     for (k in cbs)
- *       on(document, k, cbs[k], true);
- */ 
-/* 
- *     var i, l, els = document.getElementsByTagName('input');
- * 
- *     // add focus/blur 
- *     for (i = 0, l = els.length; i < l; i++) {
- *       on(els[i], 'focus', disable);
- *       on(els[i], 'blur', enable);
- *     }
- */ 
-
-    on(document, 'focus', disable, true);
-    on(document, 'blur', enable, true);
-    on(document, 'keypress', keydown, true);
-  }
-
-  if (init()) {
-    wrap();
-    enable();
-  }
+  // init searchkeys
+  init();
 })();
